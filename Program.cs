b3bc8d3d0e4491780d40da1936f997cf305c2ea7
Soporte Tecnico LitigioVirtual.com <litigiovirtualtech@hotmail.com>
2024-04-05 21:42:45 -0500

    Se agrega codigo de natillera

--- Program.cs
+++ Program.cs
@@ -31,6 +31,59 @@ namespace ParcialNumero2
             //los dos socios, también la aplicación deberá mostrar valor del préstamo(si la hicieron),
             //cuántas multas pagó en el año y el valor neto a liquidarle a ambos socios.
 
+            bool volver = true;
+            const double BONO = 0.4; //Snake Case:Notación para constantes.
+
+            while (volver)
+            {
+                decimal aporteMensual, rendimientoMensual, aporteTotal = 0, rendimientoTotal = 0, bonoMensual = 0, bonoTotal = 0, aporteTotalNeto, tasaMensual;
+                string continuar;
+
+                //Clase random
+                Random random = new Random(); //Esta es la forma de instanciar una clase en objeto
+
+                for (int mes = 1; mes <= 12; mes++)
+                {
+                    Console.Write($"Ingrese la cantidad que desea ahorrar en el mes {mes}: ");
+                    aporteMensual = Convert.ToDecimal(Console.ReadLine());
+
+                    tasaMensual = (decimal)random.Next(1, 51) / 10;
+                    rendimientoMensual = aporteMensual * (tasaMensual / 100);
+
+                    if (tasaMensual < 3.5M)
+                    {
+                        bonoMensual = aporteMensual * (decimal)BONO;
+                        bonoTotal += bonoMensual;
+                        bonoMensual = 0;
+                    }
+
+                    aporteTotal += aporteMensual;
+                    rendimientoTotal += rendimientoMensual;
+
+                    Console.Write($"MES {mes}\n" +
+                                  $"Aportes: {aporteMensual:C}\n" +
+                                  $"Tasa: {tasaMensual}%\n" +
+                                  $"Rendimientos: {rendimientoMensual:C}\n" +
+                                  $"Bono: {bonoMensual:C}\n" +
+                                  $"---------------------------------------\n" +
+                                  $" \n");
+                }
+
+                aporteTotalNeto = rendimientoTotal + aporteTotal + bonoTotal;
+
+                Console.Write($"Aportes totales: {aporteTotal:C}\n" +
+                              $"Rendimientos totales: {rendimientoTotal:C}\n" +
+                              $"Bonos totales: {bonoTotal:C}\n" +
+                              "--------------------------------\n" +
+                              $"TOTAL NETO: {aporteTotalNeto:C}\n" +
+                              $" \n");
+
+
+                Console.WriteLine("¿Desea ingresra a la natillera para el siguiente año? (s/n)");
+                continuar = Console.ReadLine().ToLower();
+                if (continuar == "n") volver = false;
+            }
+
         }
     }
 }
