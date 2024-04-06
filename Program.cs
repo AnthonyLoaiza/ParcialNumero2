985276f6013d8481ab139ff7b600830e7bc47c32
Soporte Tecnico LitigioVirtual.com <litigiovirtualtech@hotmail.com>
2024-04-05 20:26:13 -0500

    Se agregan reglas del juego

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace ParcialNumero2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Peso: 50 %
+            //Juego:
+            //Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en
+            //adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en
+            //memoria sin ser revelado.
+
+            // El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y
+            //máximo 4 integrantes.Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generará
+            //en los siguientes rangos:
+            //            -Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
+            //            - Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
+            //            - Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
+            //Cada jugador comenzará su turno intentando adivinar ese número ℕ aleatorio.El programa deberá
+            //mostrarle al jugador la siguiente información:
+            //-Si el número ingresado es mayor al número aleatorio, entonces mostrar por pantalla la
+            //palabra “MENOR” y darle la oportunidad al siguiente jugador.
+            //- Si el número ingresado es menor al número aleatorio, entonces mostrar por pantalla la
+            //palabra “MAYOR” y darle la oportunidad al siguiente jugador.
+            //- Si el número ingresado coincide con el número aleatorio, entonces mostrar por pantalla la
+            //palabra “¡HAS GANADO!”. Aquí ya finaliza el juego.
+            //El programa deberá estar en capacidad de pedir a los jugadores si desean un nuevo “tirito” para
+            //volver a jugar y borrar consola, de lo contrario, finalizar el programa.
+
+            var participantes = 0;
+
+            Console.WriteLine($"Vamos a jugar un juego.\n" +
+                $"Voy a generar un numero aleatorio que ustedes tendran que adivinar\n" +
+                $"- Si juegan 2 personas el numero se generara entre 0 y 50\n" +
+                $"- Si juegan 3 personas el numero se generara entre 0 y 100\n" +
+                $"- Si juegan 4 personas el numero se generara entre 0 y 200\n\n");
+            Console.WriteLine("Cuantas personas van a participar? (Minimo 2 Maximo 4)");
+            participantes = Convert.ToInt32(Console.ReadLine());  
+
+
+
+            var ramdon = new Random();
+            
+
         }
     }
 }
